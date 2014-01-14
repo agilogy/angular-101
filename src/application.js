93cc7dae699284c68fa8b093b96a6d2aa0464442
Jose Raya <jose.raya@agilogy.com>
2014-01-14 12:21:20 +0100

    Very simple data binding example

--- src/application.js
+++ src/application.js
@@ -1,1 +1,11 @@
 var calendar = angular.module('calendar', []);
+calendar.controller('CalendarController', function ($scope) {
+  $scope.today = new Date();
+  $scope.prev = function() {
+  	$scope.today.setDate($scope.today.getDate()-1);
+  }
+  $scope.next = function() {
+  	$scope.today.setDate($scope.today.getDate()+1);
+
+  }
+});
