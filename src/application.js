8088f361c66d86698f9f0643ef7e23ee8b754edb
Jose Raya <jose.raya@agilogy.com>
2014-01-15 17:25:58 +0100

    Added “edit calendar” Refactored the calendar service so that it remembers events across calls to “weeksFor”

--- src/application.js
+++ src/application.js
@@ -1,37 +1,40 @@
 var calendar = angular.module('calendar', ['ui.bootstrap']);
 calendar.factory('CalendarService', function() {
+	var calendar = {};
+
+	var sameDate = function(date1, date2) {
+		var res =  
+			date1.getDate() == date2.getDate() && 
+			date1.getMonth() == date2.getMonth() &&
+			date1.getYear() == date2.getYear();
+		return res;
+	};
+
 	return {
 		weeksFor: function(date) {
-			var today = new Date();
-			today.setHours(0,0,0,0)
-			var first = new Date(date.getTime());
-			first.setHours(0,0,0,0)
-			first.setDate(1);
-	       	first.setDate(-1*first.getDay() + 1);
-
-	       	//Let's find out how many weeks we need to paint (either 5 or 6)
-	       	var tmp = new Date(first.getTime());
-	       	var numWeeks = 5;
-	       	tmp.setDate(first.getDate() + 5*7);
-	       	if (tmp.getMonth() == date.getMonth()) {
-	       		numWeeks = 6;
-	       	}
-
+			var year = date.getFullYear();
+			var currentDay = new Date(date.getFullYear(), date.getMonth(), 1);
+			currentDay.setDate(-1*currentDay.getDay() + 1);
 			var result = [];
-			for(var w = 0; w < numWeeks; w++) {
-				var days = new Array();
-				for (var d=0; d < 7; d++ ) {
-					var events = [];
-					if (first.getTime() === today.getTime()) {
-						events = [{description:'Angular day @ MWC'},{description: 'Angular 101 workshop'}]
+			var showMoreWeeks = true
+			while(showMoreWeeks) {
+				var weekName = currentDay.getFullYear() + "-" + currentDay.getMonth() + "-" + currentDay.getDate();
+				if (! (weekName in calendar)) {
+					calendar[weekName] = [];
+					for (var i=0; i < 7; i++) {
+						calendar[weekName].push({
+							date: new Date(currentDay.getTime()),
+							events: []
+						});
+						currentDay.setDate(currentDay.getDate()+1);
 					}
-					days.push( {
-						date: new Date(first.getTime()),
-						events: events
-					});
-					first.setDate(first.getDate()+1);
+				} else {					
+					currentDay.setDate(currentDay.getDate()+7);
 				}
-				result.push(days);
+				result.push(calendar[weekName]);
+				var tmp = new Date(currentDay.getTime());
+				tmp.setDate(tmp.getDate()+1);
+				showMoreWeeks = date.getMonth() === tmp.getMonth();
 			}
 			return result;
 		},
@@ -83,5 +86,24 @@ calendar.controller('CalendarController', function ($scope, $modal, CalendarServ
 		modalInstance.result.then(function (event) {
 		    CalendarService.addEvent(day, event.description);
 		});
+	}
+
+	$scope.editEvent = function(day, event) {
+		var modalInstance = $modal.open({
+		    templateUrl: 'addEvent.html',
+		    controller: 'AddEventController',
+		    backdrop: false,
+		    resolve: {
+		    	title: function() { return 'Edit event'; },
+		    	event: function() {
+		    		return {date: event.date, description: event.description};
+		    	}
+		    }
+		 });
+
+		modalInstance.result.then(function (newEvent) {
+		    event.description = newEvent.description;
+		});   		
+		window.event.stopPropagation();
 	}
 });
