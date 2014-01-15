c5728e1c441f2baf36f21e2951d96e50845d7f7c
Jose Raya <jose.raya@agilogy.com>
2014-01-15 16:31:32 +0100

    Refactored some date logic to a service instantiated via a factory

--- src/application.js
+++ src/application.js
@@ -1,43 +1,47 @@
 var calendar = angular.module('calendar', []);
-calendar.controller('CalendarController', function ($scope) {
-	function weeksFor(date) {
-		var today = new Date();
-		today.setHours(0,0,0,0)
-		var first = new Date(date.getTime());
-		first.setHours(0,0,0,0)
-		first.setDate(1);
-       	first.setDate(-1*first.getDay() + 1);
+calendar.factory('CalendarService', function() {
+	return {
+		weeksFor: function(date) {
+			var today = new Date();
+			today.setHours(0,0,0,0)
+			var first = new Date(date.getTime());
+			first.setHours(0,0,0,0)
+			first.setDate(1);
+	       	first.setDate(-1*first.getDay() + 1);
 
-       	//Let's find out how many weeks we need to paint (either 5 or 6)
-       	var tmp = new Date(first.getTime());
-       	var numWeeks = 5;
-       	tmp.setDate(first.getDate() + 5*7);
-       	if (tmp.getMonth() == date.getMonth()) {
-       		numWeeks = 6;
-       	}
+	       	//Let's find out how many weeks we need to paint (either 5 or 6)
+	       	var tmp = new Date(first.getTime());
+	       	var numWeeks = 5;
+	       	tmp.setDate(first.getDate() + 5*7);
+	       	if (tmp.getMonth() == date.getMonth()) {
+	       		numWeeks = 6;
+	       	}
 
-		var result = [];
-		for(var w = 0; w < numWeeks; w++) {
-			var days = new Array();
-			for (var d=0; d < 7; d++ ) {
-				var events = [];
-				if (first.getTime() === today.getTime()) {
-					events = [{text:'Angular day @ MWC'},{text: 'Angular 101 workshop'}]
+			var result = [];
+			for(var w = 0; w < numWeeks; w++) {
+				var days = new Array();
+				for (var d=0; d < 7; d++ ) {
+					var events = [];
+					if (first.getTime() === today.getTime()) {
+						events = [{text:'Angular day @ MWC'},{text: 'Angular 101 workshop'}]
+					}
+					days.push( {
+						date: new Date(first.getTime()),
+						events: events
+					});
+					first.setDate(first.getDate()+1);
 				}
-				days.push( {
-					date: new Date(first.getTime()),
-					events: events
-				});
-				first.setDate(first.getDate()+1);
+				result.push(days);
 			}
-			result.push(days);
+			return result;
 		}
-		return result;
 	}
+})
+calendar.controller('CalendarController', function ($scope, CalendarService) {
 
 	$scope.today = new Date();
 	$scope.$watch('today.getMonth()', function() {
- 		$scope.weeks = weeksFor($scope.today);		
+ 		$scope.weeks = CalendarService.weeksFor($scope.today);		
 	}); 
 
 	$scope.prev = function() {
